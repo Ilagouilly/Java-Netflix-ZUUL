f85cb15a3f2a0292310d110df447cb17aed8a4ce
closeupapp <closeupapp@gmail.com>
2021-04-02 20:34:11 +0200

    [ILA] Initial Commit

---
+++ src/test/java/com/ismaillagouilly/zuul/JavaNetflixZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismaillagouilly.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNetflixZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
